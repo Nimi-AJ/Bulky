f49e11d8df4d503d4c32ca41f7d80f60784786aa
Nimi-AJ <toluwanimi.ajike@stu.cu.edu.ng>
2023-01-11 10:58:05 +0100

    initial

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Category> Categories
+		{
+			get; set;
+		}
+	}
+}
+
