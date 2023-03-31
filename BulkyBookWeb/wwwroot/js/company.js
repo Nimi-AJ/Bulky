eb4bb63b036f6773a530aa42210263d44f8e3819
Nimi-AJ <toluwanimi.ajike@stu.cu.edu.ng>
2023-03-31 19:04:23 +0100

    Added Index View and Ajax Data Table

--- BulkyBookWeb/wwwroot/js/company.js
+++ BulkyBookWeb/wwwroot/js/company.js
@@ -1,4 +1,4 @@
-﻿+var dataTable;
+﻿var dataTable;
 
 $(document).ready(function () {
     loadDataTable();
@@ -7,27 +7,27 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url": "/Admin/Product/GetAll"
+            "url": "/Admin/Company/GetAll"
         },
         "columns": [
-            { "data": "title", "width": "15%" },
-            { "data": "isbn", "width": "15%" },
-            { "data": "price", "width": "15%" },
-            { "data": "author", "width": "15%" },
-            { "data": "category.name", "width": "15%" },
-            { "data": "covertype.name", "width": "15%" },
+            { "data": "name", "width": "15%" },
+            { "data": "streetaddress", "width": "15%" },
+            { "data": "city", "width": "15%" },
+            { "data": "state", "width": "15%" },
+            { "data": "postalcode", "width": "15%" },
+            { "data": "phonenumber", "width": "15%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `
 
                    <div class="w-85 btn-group" role="group">
-                       <a class="btn btn-primary mx-2" href="/Admin/Product/Upsert?id=${data}"><i class="bi bi-pencil-square"></i>Edit</a>
+                       <a class="btn btn-primary mx-2" href="/Admin/Company/Upsert?id=${data}"><i class="bi bi-pencil-square"></i>Edit</a>
                    </div>
 
 
                    <div class="w-85 btn-group " role="group">
-                       <a class="btn btn-danger mx-2" onClick=Delete('/Admin/Product/DeleteProduct/${data}')  ><i class="bi bi-trash3-fill"></i>Delete</a>
+                       <a class="btn btn-danger mx-2" onClick=Delete('/Admin/Company/DeleteCompany/${data}')  ><i class="bi bi-trash3-fill"></i>Delete</a>
                    </div>
  
                           
